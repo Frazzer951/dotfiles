0b9a0370f7f36317711ca92bd7b5c15c30008323
Frazzer951 <luke343279@gmail.com>
2023-04-05 11:46:57 -0700

    add tidy and format

--- config.fish
+++ config.fish
@@ -6,6 +6,8 @@ alias cmc="cmake .. -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=on"
 alias cmb="cmake --build ."
 alias cmcf="cmake --build . --target clangformat"
 alias cmct="cmake --build . --target clangtidy"
+alias ct="run-clang-tidy -fix -p ."
+alias cf="find . -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\)' -exec clang-format -style=file -i {} \;"
 
 set PATH $HOME/.cargo/bin $PATH
 
